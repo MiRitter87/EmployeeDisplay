d59e34d6c8ee3c4e9891e47c9501d63378854f1c
Michael <michaelritter@gmx.net>
2020-11-27 11:06:58 +0100

    Fill employee data form using JSON data queried by a WebService call.

--- webapp/App.controller.js
+++ webapp/App.controller.js
@@ -6,7 +6,17 @@ sap.ui.define([
 
 	return Controller.extend("EmployeeDisplay.App", {
 		onPress : function () {
-			MessageToast.show("Mitarbeiterdaten wurden geladen.");
+			
+			var oModel = new sap.ui.model.json.JSONModel();
+			var aData = jQuery.ajax({type : "GET", contentType : "application/json", 
+				url : "http://127.0.0.1:8080/backend/services/rest/employees/1", dataType : "json", 
+				success : function(data,textStatus, jqXHR) {
+					oModel.setData({modelData : data}); // not aData
+					MessageToast.show("Mitarbeiterdaten wurden geladen.");
+				}                                                                                                              
+			});                                                                 
+			
+			this.getView().setModel(oModel);
 		}
 	});
 
